facca47d0cad91a428c011ec20db6d4c8cfea276
Floryne <floryne.tourret.pro@gmail.com>
2019-10-28 22:13:46 +0100

    frontend

--- resources/views/content/about.blade.php
+++ resources/views/content/about.blade.php
@@ -1,11 +1,12 @@
-<div class="">
+<div class="uk-panel">
+    <img class="uk-align-left uk-margin-remove-adjacent" src="storage/{{ Voyager::setting('site.background') }}" width="25%" height="auto">
     <p>Actuellement à l'école 42 Lyon, diplômée d'un DUT MMI de l'IUT de Troyes, je suis micro-entrepreneuse 
     et disponible en tant que développeur web.</p>
     <p>Spécialisée en back-end, je travaille principalement avec le framework Symfony 4. Je maitrise aussi Wordpress entre-autres. 
     Côté front, j'utilise différentes technologies qui permettent de rendre un site attractif. 
     Bien sur, ergonomie, experience utilisateur et responsive sont des indispensables lors de la conception d'un site.</p>
-    <br>
-    <br>
+    
+    
     <p>Durant mon temps libre, je m'adonne à mes passions à savoir le dessin, la photo, la lecture mais aussi et surtout la cuisine.</p>
     <p>Concernant cette dernière, 
     vous trouverez mon premier livre <i>"Pâtysserie"</i>
--- resources/views/content/websites.blade.php
+++ resources/views/content/websites.blade.php
@@ -1,7 +1,7 @@
 <div class="uk-child-width-1-3@m" uk-grid uk-scrollspy="cls: uk-animation-fade; target: .uk-card; delay: 500;">
     @foreach($websites as $post)
     <div>
-        <div class="uk-card uk-text-center project_card">
+        <div class="uk-card uk-text-center uk-card-default project_card">
             <div class="uk-inline-clip uk-transition-toggle uk-light" tabindex="0">
                 <img src="{{ Voyager::image( $post->image ) }}" alt="$post->slug">
                 <div class="uk-position-center">
